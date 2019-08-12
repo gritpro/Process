2cacda7b42716513fc1d3c0c779ddfe23a80f56c
gritzzl <51313667+gritzzl@users.noreply.github.com>
2019-08-12 11:58:45 +0800

    Update my code recording.py

--- Python/my code recording.py
+++ Python/my code recording.py
@@ -95,3 +95,105 @@ L = ['Bart', 'Lisa', 'Adam']
 for x in L:
     print('hello,%s!' %x)  # 依次按样式Hello, xxx!打印出每个名字
 
+# break语句，未跑出1、2、3……最后到10，然后打印end的效果
+n = 1
+while n <= 100:
+    if n > 10:
+        break
+    print(n)
+    n = n + 1
+print('END')  
+
+# 自己写的一个死循环
+n = 1
+while n > 0:
+    print(n)
+    n = n + 1  
+
+n1 = 255
+n2 = 1000
+print('%d的十六进制为%s' %(n1,hex(n1)),'%d的十六进制为%s' %(n2,hex(n2)))
+
+##定义函数，不明觉厉，留坑
+def quadratic(a,b,c):
+    delta = b**2-4*a*c
+    if delta < 0:
+        print('无实解')
+    else:
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)
+        return x1,x2   
+
+pow(2,pow(2,15))
+len(str(pow(2,pow(2,15))))
+
+# month.py
+months = "JanFebMarAprMayJunJulAugSepOctNovDec"
+n = input('请输入月份数：')
+pos = (int(n) - 1) * 3
+monthAbbrev = months[pos:pos+3]
+print('月份简写是'+monthAbbrev+'.')
+
+# 寻找一组数中的最大值
+n = eval(input('数字个数？'))
+max = eval(input('输入数字'))
+for i in range(n-1)
+    x = eval(input('输入数字'))
+    if x > max
+        max = x
+    print('最大的数字是',max)
+
+# def函数的演练
+def happy():
+    print("Happy Birthday to you!")
+
+def singmike():
+    happy()
+    happy()
+    print("Happy Birthday,dear Mike!")
+    happy()
+
+def singlily():
+    happy()
+    happy()
+    print("Happy Birthday,dear lily!")
+    happy()
+
+def sing(person):
+    happy()
+    happy()
+    print('Happy Birthday,dear',person + '!')
+    happy()
+
+def main():
+    sing('mike')
+    print()
+    sing('lily')
+    print()
+    sing('lucy')
+
+happy()
+sigmike()
+singlily()
+main()
+
+# 递归阶乘函数计算
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+fact(4)
+fact(100)
+
+# 字符串反转
+def reverse(s):
+    return reverse(s[1:]) + s[0] # 该函数会被报错，没有基例会无限递归
+def reverse(s):
+    if s == "":
+        return s
+    else:
+        return reverse(s[1:] + s[0])
+
+reverse('hello')
