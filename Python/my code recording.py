0b1428d2fae3cb34ab2887d2efb28f68b3e1d519
gritzzl <51313667+gritzzl@users.noreply.github.com>
2019-08-20 11:47:50 +0800

    Update my code recording.py

--- Python/my code recording.py
+++ Python/my code recording.py
@@ -371,3 +371,99 @@ for i in range(2,100):
     if is_prime(i):
         sum += i
 print(sum)
+
+# 绘制七段数码管
+import turtle
+def drawline(draw):
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    turtle.right(90)
+def drawdigit(digit):
+    drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,3,4,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,6,8] else drawline(False)
+    turtle.left(90)
+    drawline(True) if digit in [0,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,2,3,4,7,8,9] else drawline(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+def drawdate(date):
+    for i in date:
+        drawdigit(eval(i))
+def main():
+    turtle.setup(800,350,200,200)
+    turtle.penup()
+    turtle.fd(-300)
+    turtle.pensize(5)
+    drawdate('20190820')
+    turtle.hideturtle()
+    turtle.done()
+main()
+
+# 绘制七段数码管美化
+import turtle,time
+def drawgap():
+    turtle.penup()
+    turtle.fd(5)
+def drawline(draw):
+    drawgap()
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    drawgap()
+    turtle.right(90)
+def drawdigit(digit):
+    drawline(True) if digit in [2,3,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,3,4,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,6,8] else drawline(False)
+    turtle.left(90)
+    drawline(True) if digit in [0,4,5,6,8,9] else drawline(False)
+    drawline(True) if digit in [0,2,3,5,6,7,8,9] else drawline(False)
+    drawline(True) if digit in [0,1,2,3,4,7,8,9] else drawline(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+def drawdate(date):
+    turtle.pencolor("red")
+    for i in date:
+        if i == '-':
+            turtle.write("年",font = ("Arial",18,"normal"))
+            turtle.pencolor("green")
+            turtle.fd(40)
+        elif i == '=':
+            turtle.write("月", font=("Arial", 18, "normal"))
+            turtle.fd(40)
+        elif i == '+':
+            turtle.write("日", font=("Arial", 18, "normal"))
+        else:
+            drawdigit(eval(i))
+def main():
+    turtle.setup(800,350,200,200)
+    turtle.penup()
+    turtle.fd(-300)
+    turtle.pensize(5)
+    drawdate(time.strftime("%Y-%m=%d+",time.gmtime()))
+    turtle.hideturtle()
+    turtle.done()
+main()
+
+# 字符串反转
+def rvs(s):
+    if s == ""
+        return s
+    else:
+        return rvs(s[1:])+s[0]
+
+# 字符串反转简单式
+s[::-1]
+
+# 斐波那契数列
+def f(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return f(n-1) + f(n-2)
+
